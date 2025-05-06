2008cff81b7f5f35f58e8c694781b826049b6f8f
Rushabh Bhatt <rushxbh910@gmail.com>
2025-05-06 17:02:42 -0400

    ContinuousX Update

---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "chameleon_endpoint" { default = "https://chi.tacc.chameleoncloud.org:5000/v3" }
+variable "region"            { default = "CHI@TACC" }
+variable "ssh_key"           { description = "Your Chameleon SSH keypair name" }
+variable "project_id"        { description = "24" }

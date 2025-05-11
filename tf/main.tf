8b8d879c193b4b068e1b621f8484bfe2bb277956
Rushabh Bhatt <rushxbh910@gmail.com>
2025-05-11 09:33:35 -0400

    continuous X update

--- tf/main.tf
+++ tf/main.tf
@@ -3,43 +3,26 @@ locals {
   serving_node  = var.nodes["serve-1"]
 }
 
-# Training Node on KVM@TACC
+# CHI@UC - Bare metal training node (no ports, no secgroups)
 resource "openstack_compute_instance_v2" "training_node" {
-  provider     = openstack.kvm
-  name         = "eye-train-${var.suffix}-train-1"
+  provider     = openstack.uc
+  name         = "eye-train-train-1-${var.suffix}"
   flavor_name  = local.training_node.flavor
   key_pair     = var.key
   image_id     = data.openstack_images_image_v2.ubuntu.id
 
   network {
-    port = openstack_networking_port_v2.training_port.id
+    name = "sharednet1"
   }
-
-  security_groups = ["default", "eye-secgroup-${var.suffix}"]
 }
 
-resource "openstack_networking_port_v2" "training_port" {
-  provider   = openstack.kvm
-  name       = "eye-train-port-${var.suffix}-train-1"
-  network_id = data.openstack_networking_network_v2.sharednet1.id
-
-  fixed_ip {
-    subnet_id = data.openstack_networking_subnet_v2.sharednet1_subnet.id
-  }
-
-  security_group_ids = [
-    data.openstack_networking_secgroup_v2.default.id,
-    openstack_networking_secgroup_v2.eye_secgroup.id
-  ]
-}
-
-# Serving Node on KVM@TACC
+# KVM@TACC - Serving node (VM with ports and security groups)
 resource "openstack_compute_instance_v2" "serving_node" {
-  provider     = openstack.kvm
-  name         = "eye-serve-${var.suffix}-serve-1"
-  flavor_name  = local.serving_node.flavor
-  key_pair     = var.key
-  image_id     = data.openstack_images_image_v2.ubuntu.id
+  provider        = openstack.kvm
+  name            = "eye-serve-serve-1-${var.suffix}"
+  flavor_name     = local.serving_node.flavor
+  key_pair        = var.key
+  image_id        = data.openstack_images_image_v2.ubuntu.id
 
   network {
     port = openstack_networking_port_v2.serving_port.id
@@ -50,7 +33,7 @@ resource "openstack_compute_instance_v2" "serving_node" {
 
 resource "openstack_networking_port_v2" "serving_port" {
   provider   = openstack.kvm
-  name       = "eye-serve-port-${var.suffix}-serve-1"
+  name       = "eye-serve-port-serve-1-${var.suffix}"
   network_id = data.openstack_networking_network_v2.sharednet1.id
 
   fixed_ip {
@@ -61,18 +44,6 @@ resource "openstack_networking_port_v2" "serving_port" {
     data.openstack_networking_secgroup_v2.default.id,
     openstack_networking_secgroup_v2.eye_secgroup.id
   ]
-}
-
-# Floating IPs
-resource "openstack_networking_floatingip_v2" "training_fip" {
-  provider = openstack.kvm
-  pool     = "public"
-}
-
-resource "openstack_networking_floatingip_associate_v2" "training_fip_assoc" {
-  provider    = openstack.kvm
-  floating_ip = openstack_networking_floatingip_v2.training_fip.address
-  port_id     = openstack_networking_port_v2.training_port.id
 }
 
 resource "openstack_networking_floatingip_v2" "serving_fip" {
@@ -86,20 +57,19 @@ resource "openstack_networking_floatingip_associate_v2" "serving_fip_assoc" {
   port_id     = openstack_networking_port_v2.serving_port.id
 }
 
-# Security Groups
 resource "openstack_networking_secgroup_v2" "eye_secgroup" {
   provider = openstack.kvm
   name     = "eye-secgroup-${var.suffix}"
 }
 
 resource "openstack_networking_secgroup_rule_v2" "inbound" {
-  provider         = openstack.kvm
-  for_each         = toset(["22", "80", "443", "8080", "8081", "8888", "9000", "9001"])
-  direction        = "ingress"
-  ethertype        = "IPv4"
-  protocol         = "tcp"
-  port_range_min   = each.value
-  port_range_max   = each.value
-  remote_ip_prefix = "0.0.0.0/0"
+  provider          = openstack.kvm
+  for_each          = toset(["22", "80", "443", "8080", "8081", "8888", "9000", "9001"])
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = each.value
+  port_range_max    = each.value
+  remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.eye_secgroup.id
 }
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,5 +1,13 @@
-variable "key" {}
-variable "suffix" {}
+variable "suffix" {
+  type        = string
+  description = "Project/user suffix"
+}
+
+variable "key" {
+  type        = string
+  description = "SSH key name"
+}
+
 variable "nodes" {
   type = map(object({
     flavor = string

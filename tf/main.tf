f13a6a6f3d864fbbd68aaa9c51601b59a7b0f299
Rushabh Bhatt <rushxbh910@gmail.com>
2025-05-11 09:25:04 -0400

    continuous X update

--- tf/main.tf
+++ tf/main.tf
@@ -3,42 +3,24 @@ locals {
   serving_node  = var.nodes["serve-1"]
 }
 
-# --------------------
-# CHI@TACC: Training Node (GPU Bare Metal via Reservation - No Ports, No Security Groups)
-# --------------------
+# Training Node on KVM@TACC
 resource "openstack_compute_instance_v2" "training_node" {
-  provider     = openstack.chi
-  name         = "eye-train-train-1-${var.suffix}"
+  provider     = openstack.kvm
+  name         = "eye-train-${var.suffix}-train-1"
   flavor_name  = local.training_node.flavor
   key_pair     = var.key
   image_id     = data.openstack_images_image_v2.ubuntu.id
-  reservation_id = var.training_reservation_id
 
   network {
-    name = "sharednet1"
-  }
-}
-
-# --------------------
-# KVM@TACC: Serving Node (VM with Security Groups and Floating IP)
-# --------------------
-resource "openstack_compute_instance_v2" "serving_node" {
-  provider        = openstack.kvm
-  name            = "eye-serve-serve-1-${var.suffix}"
-  flavor_name     = local.serving_node.flavor
-  key_pair        = var.key
-  image_id        = data.openstack_images_image_v2.ubuntu.id
-
-  network {
-    port = openstack_networking_port_v2.serving_port.id
+    port = openstack_networking_port_v2.training_port.id
   }
 
   security_groups = ["default", "eye-secgroup-${var.suffix}"]
 }
 
-resource "openstack_networking_port_v2" "serving_port" {
+resource "openstack_networking_port_v2" "training_port" {
   provider   = openstack.kvm
-  name       = "eye-serve-port-serve-1-${var.suffix}"
+  name       = "eye-train-port-${var.suffix}-train-1"
   network_id = data.openstack_networking_network_v2.sharednet1.id
 
   fixed_ip {
@@ -49,6 +31,48 @@ resource "openstack_networking_port_v2" "serving_port" {
     data.openstack_networking_secgroup_v2.default.id,
     openstack_networking_secgroup_v2.eye_secgroup.id
   ]
+}
+
+# Serving Node on KVM@TACC
+resource "openstack_compute_instance_v2" "serving_node" {
+  provider     = openstack.kvm
+  name         = "eye-serve-${var.suffix}-serve-1"
+  flavor_name  = local.serving_node.flavor
+  key_pair     = var.key
+  image_id     = data.openstack_images_image_v2.ubuntu.id
+
+  network {
+    port = openstack_networking_port_v2.serving_port.id
+  }
+
+  security_groups = ["default", "eye-secgroup-${var.suffix}"]
+}
+
+resource "openstack_networking_port_v2" "serving_port" {
+  provider   = openstack.kvm
+  name       = "eye-serve-port-${var.suffix}-serve-1"
+  network_id = data.openstack_networking_network_v2.sharednet1.id
+
+  fixed_ip {
+    subnet_id = data.openstack_networking_subnet_v2.sharednet1_subnet.id
+  }
+
+  security_group_ids = [
+    data.openstack_networking_secgroup_v2.default.id,
+    openstack_networking_secgroup_v2.eye_secgroup.id
+  ]
+}
+
+# Floating IPs
+resource "openstack_networking_floatingip_v2" "training_fip" {
+  provider = openstack.kvm
+  pool     = "public"
+}
+
+resource "openstack_networking_floatingip_associate_v2" "training_fip_assoc" {
+  provider    = openstack.kvm
+  floating_ip = openstack_networking_floatingip_v2.training_fip.address
+  port_id     = openstack_networking_port_v2.training_port.id
 }
 
 resource "openstack_networking_floatingip_v2" "serving_fip" {
@@ -62,9 +86,7 @@ resource "openstack_networking_floatingip_associate_v2" "serving_fip_assoc" {
   port_id     = openstack_networking_port_v2.serving_port.id
 }
 
-# --------------------
-# Security Group for Serving Node
-# --------------------
+# Security Groups
 resource "openstack_networking_secgroup_v2" "eye_secgroup" {
   provider = openstack.kvm
   name     = "eye-secgroup-${var.suffix}"
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,15 +1,6 @@
-variable "key" {
-  type = string
-  description = "SSH key pair name"
-}
-
-variable "suffix" {
-  type = string
-  description = "Unique name suffix"
-}
-
+variable "key" {}
+variable "suffix" {}
 variable "nodes" {
-  description = "Map of nodes with their roles and flavors"
   type = map(object({
     flavor = string
   }))

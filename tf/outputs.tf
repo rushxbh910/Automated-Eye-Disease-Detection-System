d55999c1cf1861678404331575574c5580155d8a
Rushabh Bhatt <rushxbh910@gmail.com>
2025-05-11 17:35:27 -0400

    continuous X update

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,7 +1,3 @@
-output "serving_floating_ip" {
-  value = openstack_networking_floatingip_v2.serving_fip.address
-}
-
 output "training_node_name" {
   value = openstack_compute_instance_v2.training_node.name
 }
